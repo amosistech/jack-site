3cfd293b30dd134e53334fe03cf3890c7b554cd7
Douglas Navas <douglas@navascogt.com>
2023-11-17 22:00:26 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `jack-site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
